c4d4dcf2af4cd86803334b50f2d50680f25722e5
Leandro <lehnihon@hotmail.com>
2015-04-28 00:57:13 -0300

    hover nos botoes e ajuste no menu

--- header.php
+++ header.php
@@ -22,38 +22,31 @@
 	<div class="header-linha-verde"></div>
 	<header id="masthead" class="site-header" role="banner">
 		
-		<div class="site-branding">
-        
-        <?php if ( get_header_image() ) : ?>
-			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" class="header-image">
-		<?php endif; // End header image check. ?>
-        
-        <div class="container branding">
-			<div class="logo-organicos">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" id="site-logo" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"> 		
-					<img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/logo.png"; ?>" alt="<?php echo esc_attr( bloginfo( 'name' )); ?>">	
-				</a>
-			</div>
-			<div class="pesquisar-redes">
-				<div class="pesquisar-organicos">
-					<?php get_search_form(); ?>
+		<div class="site-branding">    
+			<div class="container branding">
+				<div class="logo-organicos">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" id="site-logo" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"> 		
+						<img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/logo.png"; ?>" alt="<?php echo esc_attr( bloginfo( 'name' )); ?>" class="header-image">	
+					</a>
 				</div>
-				<ul class="redes-sociais-header">
-					<li class="twitter"><a target="_blank" href="https://twitter.com/organicoemcasa"></a></li>
-					<li class="instagran"><a target="_blank" href="https://instagram.com/organicosnaporta"></a></li>
-					<li class="facebook"><a target="_blank" href="https://www.facebook.com/organicosnaporta"></a></li>
-				</ul>
-			</div>
-        </div><!-- .container branding-->
+				<div class="pesquisar-redes">
+					<div class="pesquisar-organicos">
+						<?php get_search_form(); ?>
+					</div>
+					<ul class="redes-sociais-header">
+						<li class="twitter"><a target="_blank" href="https://twitter.com/organicoemcasa"></a></li>
+						<li class="instagran"><a target="_blank" href="https://instagram.com/organicosnaporta"></a></li>
+						<li class="facebook"><a target="_blank" href="https://www.facebook.com/organicosnaporta"></a></li>
+					</ul>
+				</div>
+			</div><!-- .container branding-->
         </div><!-- .site-branding-->
        
 	</header><!-- #masthead -->
 <div id="content" class="site-content container">
-	<?php 
-	if (is_home()):
-		kw_sc_logo_carousel('logo_princ'); 
-	?>
+	<?php if (is_home()):?>
 		<div class=" container navmenu">
+			<?php kw_sc_logo_carousel('logo_princ'); ?>
 			<nav id="site-navigation" class="main-navigation" role="navigation">
 				<h1 class="menu-toggle"><?php _e( 'Menu', 'base' ); ?></h1>
 				<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'base' ); ?></a>
