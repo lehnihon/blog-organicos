4a0f38c7358d4b6a12ea1774d03cb7f438233e30
Leandro <lehnihon@hotmail.com>
2015-05-10 19:01:45 -0300

    footer e sidebar alterações

--- footer.php
+++ footer.php
@@ -13,9 +13,33 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="widget-footer container">
 			<img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/lousa.jpg"; ?>" />
-		</div>
-		<div class="site-info">
-			teste
+			<div class="footer-contenta">
+				<ul>
+					<li>HORÁRIO DE ATENDIMENTO</li>
+					<li>SEGUNDA A SEXTA DAS 9H ÁS 17H.</li>
+					<li>(11)3241-0818</li>
+					<li>redes</li>
+				<ul>
+			</div>
+			<div class="footer-contentb">
+				<ul>
+					<li>ORGÂNICOS NA PORTA</li>
+					<li>SOBRE O BLOG</li>
+					<li>CONHEÇA NOSSA LOJA</li>
+					<li>FALE CONOSCO</li>
+				<ul>			
+			</div>
+			<div class="footer-contentc">
+				<ul>
+					<li>BLOG</li>
+					<li>NUTRIÇÃO ESPORTIVA</li>
+					<li>ALIMENTOS ORGÂNICOS</li>
+					<li>ALIMENTAÇÃO SAUDÁVEL</li>
+					<li>DICAS E RECEITAS</li>
+				<ul>
+			</div>
+			<div style="clear:both"></div>
+			<img class="img-footer" src=<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/footer_fruta.png"; ?> />
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
