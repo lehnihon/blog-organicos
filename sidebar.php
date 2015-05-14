42009a6cdc55261518a91202a39122fc54470230
Leandro <leandro@seepix.com.br>
2015-05-14 18:19:40 -0300

    codigos de plugins alterados

--- sidebar.php
+++ sidebar.php
@@ -8,16 +8,16 @@
 <div id="secondary" class="widget-area four columns" role="complementary">
 	<?php do_action( 'before_sidebar' ); ?>
 		<aside id="archives" class="widget">
-			PUBLICIDADE
+			<?php echo do_shortcode("[metaslider id=34]"); ?>
 		</aside>
 		
 		<aside id="archives" class="widget">
 			<img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/newsletter_header.png"; ?>"/>
-			<?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 1 ); } ?>
+			<?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 5 ); } ?>
 		</aside>
 		<aside id="archives" class="widget">
 			<ul>
-				<div class="fb-like" data-href="https://www.facebook.com/organicosnaporta" data-width="200" data-layout="standard" data-action="like" data-show-faces="true" data-share="false"></div>
+				<div class="fb-like-box" data-href="https://www.facebook.com/organicosnaporta" data-width="250" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
 			</ul>
 		</aside>		
 
